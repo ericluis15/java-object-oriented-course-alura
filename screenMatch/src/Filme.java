c2d94062133496d3fe48f503e4e27538126ca2e0
ericluis15 <eric.strumendo@ab-inbev.com>
2023-07-09 23:32:57 -0300

    feat: add class Filme

---
+++ screenMatch/src/Filme.java
@@ -0,0 +1,23 @@
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double somaDasAvaliacoes;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome: "+this.nome);
+        System.out.println("Ano De Lancamento: "+this.anoDeLancamento);
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
+}
